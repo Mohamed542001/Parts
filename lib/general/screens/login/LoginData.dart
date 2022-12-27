8c7aed15787d82f7298a3a93119c48e2c09e82fe
AyaHamed <aya.hamed181@gmail.com>
2022-12-27 20:00:21 +0400

    auth api

--- lib/general/screens/login/LoginData.dart
+++ lib/general/screens/login/LoginData.dart
@@ -16,15 +16,24 @@ class LoginData {
   // methods
   void userLogin(BuildContext context) async {
     FocusScope.of(context).requestFocus(FocusNode());
+
+    // check validation
     if (formKey.currentState!.validate()) {
       btnKey.currentState!.animateForward();
+
+      // convert arabic numbers to english
       String passEn = HelperMethods.convertDigitsToLatin(password.text);
+
+      // call api
       bool data = await GeneralRepository(context).setUserLogin(phone.text, passEn);
       btnKey.currentState!.animateReverse();
+
+      // navigation action when user is logged in successfully
       if(data == true){
         // AutoRouter.of(context).push(BuyerHomeRoute());
         print("=========================>>> true");
       }
+
     }
   }
 }
